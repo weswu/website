0623438984584916fe532e70174256590f842f94
weswu <836790625@qq.com>
2020-09-25 15:30:37 +0800

    Squashed 'ai/' content from commit bc48a8a
    git-subtree-dir: ai
    git-subtree-split: bc48a8a0d2bc57d214de841d83cd545e49305b5b

---
+++ src/components/home/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-footer
+    id="home-footer"
+    color="grey darken-4"
+    dark
+    min-height="72"
+  >
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6">
+          <div class="d-flex flex-wrap justify-md-start justify-center justify-md-none">
+            <template v-for="(s, i) in social">
+              <a
+                :key="s.name"
+                class="white--text pa-1 pa-md-0"
+                :href="s.url"
+                v-text="s.name"
+              />
+
+              <v-responsive
+                v-if="i < social.length - 1"
+                :key="`divider-${s}`"
+                class="mx-4 shrink hidden-sm-and-down"
+                max-height="24"
+              >
+                <v-divider vertical />
+              </v-responsive>
+            </template>
+          </div>
+        </v-col>
+
+        <v-col cols="12" md="6" class="text-center text-md-right">
+          Copyright &copy; 2020 Vuetify, LLC
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'HomeFooter',
+    data: () => ({
+      social: [
+        {name: 'Weibo', url: 'https://weibo.com/shevywu'},
+        {name: 'GitHub', url: 'https://github.com/weswu'},
+      ],
+    }),
+  }
+</script>
+
+<style lang="sass">
+  #home-footer a
+    text-decoration: none
+</style>
---
+++ src/components/home/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <home-app-bar />
+    <home-view />
+    <home-footer />
+    <home-settings />
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'HomeLayout',
+    components: {
+      HomeAppBar: () => import('./AppBar'),
+      HomeFooter: () => import('./Footer'),
+      HomeSettings: () => import('./Settings'),
+      HomeView: () => import('./View'),
+    },
+  }
+</script>
